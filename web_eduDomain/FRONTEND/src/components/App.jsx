ff4c751ba5727f9225383d72d258018db7c774ca
zuza <115052199+craffulz@users.noreply.github.com>
2024-10-08 20:48:08 +0200

    dfk

--- web_eduDomain/FRONTEND/src/components/App.jsx
+++ web_eduDomain/FRONTEND/src/components/App.jsx
@@ -1,7 +1,7 @@
 import "../styles/App.scss";
 import { Analytics } from "@vercel/analytics/react";
 import Home from "../pages/home.jsx";
-import Patrimoine from "../pages/patrimoine.jsx";
+//import Patrimoine from "../pages/patrimoine.jsx";
 import Contact from "../pages/contact.jsx";
 import Support from "../pages/support.jsx";
 import Formation from "../pages/formation.jsx";
@@ -32,15 +32,15 @@ const router = createBrowserRouter([
       </>
     ),
   },
-  {
-    path: "/patrimoine",
-    element: (
-      <>
-        <ScrollRestoration />
-        <Patrimoine />
-      </>
-    ),
-  },
+  // {
+  //   path: "/patrimoine",
+  //   element: (
+  //     <>
+  //       <ScrollRestoration />
+  //       <Patrimoine />
+  //     </>
+  //   ),
+  // },
   {
     path: "/contact",
     element: (
