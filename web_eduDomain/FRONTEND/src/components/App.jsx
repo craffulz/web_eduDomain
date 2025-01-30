cebdc73441cf7d3fcfda62bcc36b530dbe7a7c7d
zuza <115052199+craffulz@users.noreply.github.com>
2025-01-30 08:51:42 +0100

    route 'contact' issue fixed

--- web_eduDomain/FRONTEND/src/components/App.jsx
+++ web_eduDomain/FRONTEND/src/components/App.jsx
@@ -2,7 +2,7 @@ import "../styles/App.scss";
 //import { Analytics } from "@vercel/analytics/react";
 import Home from "../pages/home.jsx";
 import Patrimoine from "../pages/Patrimoine.jsx";
-import Contact from "../pages/Contact.jsx";
+import Contact from "../pages/contact.jsx";
 import Support from "../pages/Support.jsx";
 import Formation from "../pages/Formation.jsx";
 import Faqs from "../pages/Faqs.jsx";
